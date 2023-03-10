d692c8bc69242fca5f9e75b7e6a0614fbd4b0c6d
Klaudia Miłoszewska <klaudiamiloszewska100@gmail.com>
2023-03-10 21:11:26 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import TaskItem from "./components/TaskItem";
 import './styles/index.css';
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addTask, deleteTask, updateTaskName } from "./features/Tasks";
-import AddIcon from '@mui/icons-material/Add';
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import SaveAltIcon from '@mui/icons-material/SaveAlt';
 
 function App() {
   const dispatch = useDispatch();
   const taskList = useSelector((state) => state.tasks.value);
   const [text, setText] = useState("");
   const [newText, setNewText] = useState("");
-
+  const [isEdited, setIsEdited] = useState(false);
+  const newTaskInput = useRef(null);
+  const handleInputText = (event) => {
+    setText(event.target.value);
+  }
+  const handleInputNewText = (event) => {
+    setNewText(event.target.value);
+  }
+  const handleAddButton = () => {
+    dispatch(addTask({id: taskList[taskList.length -1].id + 1, text: text}));
+    newTaskInput.current.value = null;
+  }
   return (
     <div className="app">
       <div className="heading">
         <h1>Todo List</h1>
         <div className="input-container">
-          <input type="text" placeholder="Write your new task.." />
-          <button><AddIcon color="primary"/></button>
+          <input type="text" placeholder="Write your new task.." onChange={handleInputText} ref={newTaskInput}/>
+          <button onClick={handleAddButton}><AddRoundedIcon fontSize="large" sx={{color: "white"}}/></button>
         </div>
+      </div>
+      <div className="tasks-list">
+        {taskList.map((task) => {
+          return(
+            <div className="task-element">
+              <h2 className={isEdited ? 'hide' : ''}>{task.text}</h2>
+              <input className={isEdited ? '' : 'hide'} type="text" value={task.text} onChange={handleInputNewText}/>                          
+              <div className="buttons">
+              <button className={isEdited ? '' : 'hide'} onClick={() => {dispatch(updateTaskName({id: task.id, text: newText})); setIsEdited(!isEdited);}}>
+                <SaveAltIcon fontSize="small" className="icon"/>
+              </button>
+                <button onClick={() => {dispatch(deleteTask({id: task.id})); }}>
+                  <DeleteIcon fontSize="small" className="icon"/>
+                </button>
+                <button onClick={() => { setIsEdited(!isEdited) }}>
+                  <EditIcon fontSize="small" className="icon"/>
+                </button>
+              </div>
+            </div>
+            )
+        })}
       </div>
     </div>
   );
--- src/features/Tasks.jsx
+++ src/features/Tasks.jsx
@@ -1,17 +1,21 @@
-import React from "react";
 import { createSlice } from "@reduxjs/toolkit"; 
 
 export const taskSlice = createSlice({
     name: "tasks",
-    initialState: {
+    initialState: {value : [{id: 0, text: "text"},]},
+    reducers: {
         addTask: (state, action) => {
-            
+            state.value.push(action.payload);
         },
         deleteTask: (state, action) => {
-
+            state.value = state.value.filter((task) => task.id !== action.payload.id);
         },
         updateTaskName: (state, action) => {
-
+            state.value.map((task) => {
+                if(task.id === action.payload.id){
+                    task.text = action.payload.text;
+                }
+            })
         },
     }
 })
